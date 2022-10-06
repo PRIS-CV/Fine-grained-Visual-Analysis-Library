5ba6fd62cf5b6d993dd4cf7b5844a7e627c94da9
xin-ran-w <wangxr@bupt.edu.cn>
2022-10-06 23:49:56 +0800

    add comments

--- fgvclib/apis/build.py
+++ fgvclib/apis/build.py
@@ -7,7 +7,9 @@
 import torch
 from torch import nn
 import torch.optim as optim
+from torch.optim.optimizer import Optimizer
 from torchvision import transforms
+from torch.utils.data import DataLoader
 import typing as t
 from yacs.config import CfgNode
 
@@ -27,6 +29,13 @@ from fgvclib.metrics import NamedMetric
 
 
 def build_model(model_cfg: CfgNode) -> nn.Module:
+    r"""Build a FGVC model according to config.
+
+    Args:
+        model_cfg (CfgNode): The model config node of root config.
+    Returns:
+        nn.Module: The FGVC model.
+    """
 
     backbone_builder = get_backbone(model_cfg.BACKBONE.NAME)
     backbone = backbone_builder(cfg=tltd(model_cfg.BACKBONE.ARGS))
@@ -56,24 +65,55 @@ def build_model(model_cfg: CfgNode) -> nn.Module:
     return model
 
 def build_logger(cfg: CfgNode) -> Logger:
+    r"""Build a Logger object according to config.
+
+    Args:
+        cfg (CfgNode): The root config node.
+    Returns:
+        Logger: The Logger object.
+    """
+
     return get_logger(cfg.LOGGER.NAME)(cfg)
 
-def build_transforms(transforms_cfg: CfgNode):
+def build_transforms(transforms_cfg: CfgNode) -> transforms.Compose:
+    r"""Build transforms for train or test dataset according to config.
+
+    Args:
+        transforms_cfg (CfgNode): The root config node.
+    Returns:
+        transforms.Compose: The transforms.Compose object in Pytorch.
+    """
+
     return transforms.Compose([get_transform(item['name'])(item) for item in transforms_cfg])
 
-def build_dataset(root:str, cfg: CfgNode, transforms):
+def build_dataset(root:str, cfg: CfgNode, transforms) -> DataLoader:
+    r"""Build a dataloader for training or evaluation.
+
+    Args:
+        root (str): The directory of dataset.
+        cfg (CfgNode): The root config node.
+    Returns:
+        DataLoader: A Pytorch Dataloader.
+    """
 
     dataset = Dataset_AnnoFolder(root=root, transform=transforms, positive=cfg.POSITIVE)
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=cfg.BATCH_SIZE, shuffle=cfg.SHUFFLE, num_workers=cfg.NUM_WORKERS)
 
     return data_loader
 
-def build_optimizer(optim_cfg: CfgNode, model):
+def build_optimizer(optim_cfg: CfgNode, model:t.Union[nn.Module, nn.DataParallel]) -> Optimizer:
+    r"""Build a optimizer for training.
+
+    Args:
+        optim_cfg (CfgNode): The optimizer config node of root config node.
+    Returns:
+        Optimizer: A Pytorch Optimizer.
+    """
 
     params= list()
     model_attrs = ["backbone", "encoding", "necks", "heads"]
 
-    if isinstance(model, torch.nn.DataParallel):
+    if isinstance(model, nn.DataParallel):
         for attr in model_attrs:
             if getattr(model.module, attr) and optim_cfg.LR[attr]:
                 params.append({
@@ -92,15 +132,39 @@ def build_optimizer(optim_cfg: CfgNode, model):
     
     return optimizer
 
-def build_criterion(criterion_cfg: CfgNode):
+def build_criterion(criterion_cfg: CfgNode) -> nn.Module:
+    r"""Build loss function for training.
+
+    Args:
+        criterion_cfg (CfgNode): The criterion config node of root config node.
+    Returns:
+        nn.Module: A loss function.
+    """
+
     criterion_builder = get_criterion(criterion_cfg['name'])
     criterion = criterion_builder(cfg=tltd(criterion_cfg['args']))
     return criterion
 
-def build_interpreter(model, cfg: CfgNode) -> Interpreter:
+def build_interpreter(model: nn.Module, cfg: CfgNode) -> Interpreter:
+    r"""Build loss function for training.
+
+    Args:
+        cfg (CfgNode): The root config node.
+    Returns:
+        Interpreter: A Interpreter.
+    """
+
     return get_interpreter(cfg.INTERPRETER.NAME)(model, cfg)
 
 def build_metrics(metrics_cfg: CfgNode, use_cuda:bool=True) -> t.List[NamedMetric]:
+    r"""Build metrics for evaluation.
+
+    Args:
+        metrics_cfg (CfgNode): The criterion config node of root config node.
+    Returns:
+        t.List[NamedMetric]: A List of NamedMetric.
+    """
+
     metrics = []
     for cfg in metrics_cfg:
         metric = get_metric(cfg["metric"])(name=cfg["name"], top_k=cfg["top_k"], threshold=cfg["threshold"])
