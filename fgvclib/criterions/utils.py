f5ad3c571a22df06be282e5506517115dce054cc
xin-ran-w <wangxr@bupt.edu.cn>
2022-10-07 12:46:59 +0800

    update comment

--- fgvclib/criterions/utils.py
+++ fgvclib/criterions/utils.py
@@ -4,20 +4,40 @@ from torch import Tensor
 
 @dataclass
 class LossItem:
+    r"""A dataclass object for store training loss
+        Args:
+            name (string): The loss item name.
+            value (torch.Tensor): The value of loss.
+            weight (float, optional): The weight of current loss item, default is 1.0. 
+    """
+
     name: str = ""
-    value: t.Any = None
-    weight: t.Optional[t.Union[float, Tensor]] = 1.0
+    value: Tensor = None
+    weight: t.Optional[float] = 1.0
 
 def compute_loss_value(items: t.List[LossItem]) -> Tensor:
-        """
-        Should return the value that will be used later for backward propagation
-        """
-        total = 0.
-        for item in items:
-            total = total + item.weight * item.value
-        return total
+    r"""A dataclass object for store training loss
+        Args:
+            items (List[LossItem]): The loss items.
+        
+        Return:
+            Tensor: The total loss value.
+    """
+
+    total = 0.
+    for item in items:
+        total = total + item.weight * item.value
+    return total
 
 def detach_loss_value(items: t.List[LossItem]) -> t.Dict:
+    r"""Detach loss value from GPU.
+        Args:
+            items (List[LossItem]): The loss items.
+        
+        Return:
+            Dict: A loss information dict whose key is loss name, value is loss value.
+    """
+    
     loss_dict = {}
     for item in items:
         loss_dict.update({item.name: round(item.value.item(), 2)})
